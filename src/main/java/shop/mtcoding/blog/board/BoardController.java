59bccbb036eab55ac084d579d0b90c007d53d449
getinthere <getinthere@naver.com>
2024-03-18 09:15:40 +0900

    JPA Repository 준비완료

---
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -0,0 +1,94 @@
+package shop.mtcoding.blog.board;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import shop.mtcoding.blog._core.errors.exception.Exception403;
+import shop.mtcoding.blog._core.errors.exception.Exception404;
+import shop.mtcoding.blog.user.User;
+
+import java.util.List;
+
+@RequiredArgsConstructor // final이 붙은 친구들의 생성자를 만들어줘
+@Controller // new BoardController(IoC에서 BoardRepository를 찾아서 주입) -> IoC 컨테이너 등록
+public class BoardController {
+
+    private final BoardRepository boardRepository;
+    private final HttpSession session;
+
+    @PostMapping("/board/save")
+    public String save(BoardRequest.SaveDTO reqDTO) {
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        boardRepository.save(reqDTO.toEntity(sessionUser));
+        return "redirect:/";
+    }
+
+    @PostMapping("/board/{id}/update")
+    public String update(@PathVariable Integer id, BoardRequest.UpdateDTO reqDTO) {
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        Board board = boardRepository.findById(id);
+
+        if(sessionUser.getId() != board.getUser().getId()){
+            throw new Exception403("게시글을 수정할 권한이 없습니다");
+        }
+
+        boardRepository.updateById(id, reqDTO.getTitle(), reqDTO.getContent());
+        return "redirect:/board/" + id;
+    }
+
+    @GetMapping("/board/{id}/update-form")
+    public String updateForm(@PathVariable Integer id, HttpServletRequest request) {
+        Board board = boardRepository.findById(id);
+
+        request.setAttribute("board", board);
+        return "board/update-form";
+    }
+
+    @PostMapping("/board/{id}/delete")
+    public String delete(@PathVariable Integer id) {
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        Board board = boardRepository.findById(id);
+
+        if(sessionUser.getId() != board.getUser().getId()){
+            throw new Exception403("게시글을 삭제할 권한이 없습니다");
+        }
+
+        boardRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/")
+    public String index(HttpServletRequest request) {
+        List<Board> boardList = boardRepository.findAll();
+        request.setAttribute("boardList", boardList);
+        return "index";
+    }
+
+    @GetMapping("/board/save-form")
+    public String saveForm() {
+        return "board/save-form";
+    }
+
+    @GetMapping("/board/{id}")
+    public String detail(@PathVariable Integer id, HttpServletRequest request) {
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        Board board = boardRepository.findByIdJoinUser(id);
+
+        // 로그인을 하고, 게시글의 주인이면 isOwner가 true가 된다.
+        boolean isOwner = false;
+        if(sessionUser != null){
+            if(sessionUser.getId() == board.getUser().getId()){
+                isOwner = true;
+            }
+        }
+
+        request.setAttribute("isOwner", isOwner);
+        request.setAttribute("board", board);
+        return "board/detail";
+    }
+}
---
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -0,0 +1,76 @@
+package shop.mtcoding.blog.user;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import shop.mtcoding.blog._core.errors.exception.Exception400;
+import shop.mtcoding.blog._core.errors.exception.Exception401;
+
+
+@RequiredArgsConstructor
+@Controller
+public class UserController {
+
+    private final UserRepository userRepository;
+    private final HttpSession session;
+
+    @PostMapping("/user/update")
+    public String update(UserRequest.UpdateDTO reqDTO) {
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        User newSessionUser = userRepository.updateById(sessionUser.getId(), reqDTO.getPassword(), reqDTO.getEmail());
+        session.setAttribute("sessionUser", newSessionUser);
+        return "redirect:/";
+    }
+
+    @PostMapping("/join")
+    public String join(UserRequest.JoinDTO reqDTO) {
+        try {
+            userRepository.save(reqDTO.toEntity());
+        } catch (DataIntegrityViolationException e) {
+            throw new Exception400("동일한 유저네임이 존재합니다");
+        }
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/login")
+    public String login(UserRequest.LoginDTO reqDTO) {
+        try {
+            User sessionUser = userRepository.findByUsernameAndPassword(reqDTO.getUsername(), reqDTO.getPassword());
+            session.setAttribute("sessionUser", sessionUser);
+            return "redirect:/";
+        }catch (EmptyResultDataAccessException e){
+            throw new Exception401("유저네임 혹은 비밀번호가 틀렸어요");
+        }
+    }
+
+    @GetMapping("/join-form")
+    public String joinForm() {
+        return "user/join-form";
+    }
+
+    @GetMapping("/login-form")
+    public String loginForm() {
+        return "user/login-form";
+    }
+
+    @GetMapping("/user/update-form")
+    public String updateForm(HttpServletRequest request) {
+        User sessionUser = (User) session.getAttribute("sessionUser");
+
+        User user = userRepository.findById(sessionUser.getId());
+        request.setAttribute("user", user);
+        return "user/update-form";
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        session.invalidate();
+        return "redirect:/";
+    }
+}
